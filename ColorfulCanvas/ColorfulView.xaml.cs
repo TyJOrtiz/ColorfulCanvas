77dbbbe3e47a8f5db0d66d25ba6ed9ba20a21999
Tyler Ortiz <imthewiseguy@outlook.com>
2025-03-01 19:48:01 -0800

    Add project files.

---
+++ ColorfulCanvas/ColorfulView.xaml.cs
@@ -0,0 +1,187 @@
+﻿using CommunityToolkit.WinUI.Media;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI;
+using ColorThiefDotNet;
+using Color = Windows.UI.Color;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ColorfulCanvas
+{
+    public sealed partial class ColorfulView : UserControl
+    {
+        public ColorfulView()
+        {
+            this.InitializeComponent();
+            this.Loaded += ColorfulView_Loaded;
+        }
+        int count = 100;
+        public List<Color> Colors { get; set; } = new List<Color>()
+        {
+
+            Color.FromArgb(255, (byte)(0.9586862922 * 255), (byte)(0.660125792 * 255), (byte)(0.8447988033 * 255)),
+            Color.FromArgb(255, (byte)(0.8714533448 * 255), (byte)(0.723166883 * 255), (byte)(0.9342088699 * 255)),
+            Color.FromArgb(255, (byte)(0.7458761334 * 255), (byte)(0.7851135731 * 255), (byte)(0.9899476171 * 255)),
+            Color.FromArgb(255, (byte)(0.4398113191 * 255), (byte)(0.8953480721 * 255), (byte)(0.9796616435 * 255)),
+            Color.FromArgb(255, (byte)(0.3484552801 * 255), (byte)(0.933657825 * 255), (byte)(0.9058339596 * 255)),
+            Color.FromArgb(255, (byte)(0.5567936897 * 255), (byte)(0.9780793786 * 255), (byte)(0.6893508434 * 255))
+        };
+        private void ColorfulView_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this.Background = new SolidColorBrush(Colors[0]);
+            Canvas1.Children.Clear();
+            var r = new Random();
+            while (Colors.Count < count)
+            {
+                Colors.AddRange(Colors.OrderBy(x => r.Next()).ToList());
+            }
+            if (Colors.Count > count)
+            {
+                while (Colors.Count > count)
+                {
+                    Colors.RemoveAt(Colors.Count - 1);
+                }
+            }
+            Debug.Assert(Colors.Count == count);
+            var list1 = new List<(double, double, double)>();
+            while (list1.Count < count)
+            {
+                var config = Randomize(this.ActualSize);
+                if (hasher.Add(config))
+                {
+                    list1.Add(config);
+                }
+            }
+            foreach (var h in hasher)
+            {
+                var item = GenerateItem(h, hasher.ToList().IndexOf(h));
+                Canvas1.Children.Add(item);
+                GenerateAnimation(item);
+            }
+            this.SizeChanged += ColorfulView_SizeChanged;
+        }
+
+        private void ColorfulView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            hasher.Clear();
+            Canvas1.Children.Clear();
+            var r = new Random();
+            while (Colors.Count < count)
+            {
+                Colors.AddRange(Colors.OrderBy(x => r.Next()).ToList());
+            }
+            if (Colors.Count > count)
+            {
+                while (Colors.Count > count)
+                {
+                    Colors.RemoveAt(Colors.Count - 1);
+                }
+            }
+            Debug.Assert(Colors.Count == count);
+            var list1 = new List<(double, double, double)>();
+            while (list1.Count < count)
+            {
+                var config = Randomize(this.ActualSize);
+                if (hasher.Add(config))
+                {
+                    list1.Add(config);
+                }
+            }
+            foreach (var h in hasher)
+            {
+                var item = GenerateItem(h, hasher.ToList().IndexOf(h));
+                Canvas1.Children.Add(item);
+                GenerateAnimation(item);
+
+            }
+        }
+        private DispatcherTimer Timer;
+        private void GenerateAnimation(UIElement item, DispatcherTimer t = null)
+        {
+            var ellpise = item as Ellipse;
+            Storyboard storyboard = new Storyboard();
+
+            var ransom = new Random();
+            var color = Colors[ransom.Next(Colors.Count)];
+            var animation = new Windows.UI.Xaml.Media.Animation.ColorAnimation();
+            animation.BeginTime = TimeSpan.FromSeconds(0);
+            animation.Duration = TimeSpan.FromSeconds(5);
+            animation.EnableDependentAnimation = true;
+            animation.From = (ellpise.Fill as SolidColorBrush).Color;
+            animation.To = color;
+            storyboard.Children.Add(animation);
+            Storyboard.SetTarget(animation, ellpise);
+            Storyboard.SetTargetProperty(animation, "(Ellipse.Fill).(SolidColorBrush.Color)");
+            animation.Completed += (a, c) =>
+            {
+                GenerateAnimation(item);
+            };
+            storyboard.Begin();
+        }
+
+        private UIElement GenerateItem((double, double, double) h, int v)
+        {
+            var ellipse = new Ellipse
+            {
+                Width = h.Item1,
+                Height = h.Item1,
+                Fill = new SolidColorBrush(Colors[v]),
+            };
+            Canvas.SetLeft(ellipse, (this.ActualWidth / 2) + h.Item2 - (ellipse.Width / 2));
+            Canvas.SetTop(ellipse, (this.ActualHeight / 2) + h.Item3 - (ellipse.Width / 2));
+            return ellipse;
+        }
+
+        private (double, double, double) Randomize(Vector2 actualSize)
+        {
+            double diameter = 0;
+            double offsetX = 0;
+            double offsetY = 0;
+            var decision = (actualSize.X + actualSize.Y) / 4;
+            var andom = new Random();
+            diameter = NextDouble(andom, decision * .25, decision * .75);
+            offsetX = NextDouble(andom, -(actualSize.X / 2), (actualSize.X / 2));
+            offsetY = NextDouble(andom, -(actualSize.Y / 2), (actualSize.Y / 2)); 
+            return (diameter, offsetX, offsetY);
+        }
+
+        public double NextDouble(Random RandGenerator, double MinValue, double MaxValue)
+        {
+            return RandGenerator.NextDouble() * (MaxValue - MinValue) + MinValue;
+        }
+        public HashSet<(double, double, double)> hasher = new System.Collections.Generic.HashSet<(double, double, double)>();
+
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            ((Grid)sender).Background = new BackdropBlurBrush
+            {
+                Amount = 64,
+                Opacity = 1,
+                FallbackColor = Windows.UI.Colors.Transparent
+            };
+        }
+    }
+}
---
+++ ColorfulCanvas/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ColorThiefDotNet;
+using CommunityToolkit.WinUI.Animations;
+using CommunityToolkit.WinUI.Media;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using Color = Windows.UI.Color;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ColorfulCanvas
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
